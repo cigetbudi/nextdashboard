4e7b2d5e6fefaad620d42d0ae2a175d0b3f91181
cigetbudi <cigetbudi@gmail.com>
2023-11-11 15:42:09 +0700

    Add Header Component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Header from '@/components/Header'
 import Head from 'next/head'
 import Image from 'next/image'
 
@@ -11,7 +12,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">
+      <main className="bg-gray-100 min-h-screen">
+        <Header />
       </main>
     </>
   )
