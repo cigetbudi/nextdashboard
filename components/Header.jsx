cd0cbd4ef68b9568385bede24475a7ac805c1fe3
cigetbudi <cigetbudi@gmail.com>
2023-11-11 21:12:05 +0700

    Add TopCards

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='flex justify-between px-4 pt-4'>
       <h2>Dashboard</h2>
-      <h2>Selamat Datang</h2>
+      <h2>Selamat Datang, Anjeng</h2>
     </div>
   );
 };
